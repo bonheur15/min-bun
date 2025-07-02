3b02fde97cc4bd34aa4d7bd415e8bbd8dccdf487
Rumanzi Bonheur <35499564+bonheur15@users.noreply.github.com>
2025-07-02 13:07:03 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -80,7 +80,7 @@ app.use(express.json());
  */
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({ 
-        message: 'Welcome to the File Permission and CPU Load API! cool',
+        message: 'Welcome to the File Permission and CPU Load API! ...',
         endpoints: {
             demo: 'GET /demo',
             cpu: 'GET /cpu'
